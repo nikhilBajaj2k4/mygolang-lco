7e492a53dddbf42de38fb64298b828bb03c5da78
nikhilbajaj2k4 <nbajaj133@gmail.com>
2022-05-05 09:10:09 +0530

    Update 2

--- randomPractice/main.go
+++ randomPractice/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -11,16 +12,24 @@ func main() {
 	content := "Ye ek piece of content he, writing to be specific"
 	
 	file, err := os.Create("./file.txt")
-	if err != nil {
-		panic(err)
-	}
+	errorCheck(err)
 
 	length, err := io.WriteString(file, content)
-	if err != nil {
-		panic(err)
-	}
+	errorCheck(err)
 
 	fmt.Println("Length is ", length)
 	defer file.Close()
+	readFile("./file.txt")
+} 
+ 
+func readFile(filename string){
+	databyte, err := ioutil.ReadFile(filename)
+	errorCheck(err)
+	fmt.Println("Content of the file is \n", string(databyte))
 }
  
+func errorCheck(err error){
+	if err != nil {
+		panic(err)
+	}
+}
